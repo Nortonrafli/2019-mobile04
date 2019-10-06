881610ac22dcf55938c73c09648e57df45c7aacb
nortonrafli <norton.nra27@gmail.com>
2019-10-06 23:48:48 +0700

    menambahkan code di masing masing activity

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -1,19 +1,17 @@
 package id.ac.polinema.intent;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import id.ac.polinema.intent.model.User;
 
 public class ProfileParcelableActivity extends AppCompatActivity {
-
-
-    private TextView output_username;
-    private TextView output_name;
-    private TextView output_age;
-    User user = new User();
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,20 +19,20 @@ public class ProfileParcelableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_parcelable);
 
         // TODO: bind here
-        output_username = findViewById(R.id.text_username);
-        output_name = findViewById(R.id.text_name);
-        output_age = findViewById(R.id.text_age);
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
-        user = getIntent().getParcelableExtra("user");
-        if (user!= null) {
+        Intent intent =getIntent();
+        User parcel = intent.getParcelableExtra("user");
 
-
-            output_username.setText(user.getUsername());
-            output_age.setText(user.getAge());
-            output_name.setText(user.getName());
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
 
             // TODO: display value here
-
+            usernameText.setText(parcel.getUsername());
+            nameText.setText(parcel.getName());
+            ageText.setText(String.valueOf(parcel.getAge()));
         }
     }
 }
